1d40eb4480acf0eb293f0e79b3bceaf7c10d3412
Logan Alvarez <logan.alvarez616@gmail.com>
2022-02-04 15:49:19 -0500

    Updated CRUD operations with try/catch block

--- functions/src/routes/IcebreakerRoute.ts
+++ functions/src/routes/IcebreakerRoute.ts
@@ -7,40 +7,52 @@ const IcebreakerRoute = express.Router();
 
 //GET
 IcebreakerRoute.get("/icebreakers", async (req, res) => {
-  const client = await getClient();
-  const result = await client
-    .db()
-    .collection<IceBreaker>("icebreakers")
-    .find()
-    .toArray();
-  res.json(result).status(200);
+  try {
+    const client = await getClient();
+    const result = await client
+      .db()
+      .collection<IceBreaker>("icebreakers")
+      .find()
+      .toArray();
+    res.json(result).status(200);
+  } catch (err) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 
 //GET BY ID
 IcebreakerRoute.get("/icebreakers/:id", async (req, res) => {
   const id = req.params.id;
-  const client = await getClient();
-  const result = await client
-    .db()
-    .collection<IceBreaker>("icebreakers")
-    .findOne({ _id: new ObjectId(id) });
-  if (result) {
-    res.json(result);
-  } else {
-    res.status(404).json({ message: "Not Found" });
+  try {
+    const client = await getClient();
+    const result = await client
+      .db()
+      .collection<IceBreaker>("icebreakers")
+      .findOne({ _id: new ObjectId(id) });
+    if (result) {
+      res.json(result);
+    } else {
+      res.status(404).json({ message: "Not Found" });
+    }
+  } catch (err) {
+    res.status(500).json({ message: "Internal Server Error" });
   }
 });
 
 //POST
 IcebreakerRoute.post("/icebreakers", async (req, res) => {
   let newIceBreaker = req.body as IceBreaker;
-  const client = await getClient();
-  await client
-    .db()
-    .collection<IceBreaker>("icebreakers")
-    .insertOne(newIceBreaker);
-  res.status(201);
-  res.json(newIceBreaker);
+  try {
+    const client = await getClient();
+    await client
+      .db()
+      .collection<IceBreaker>("icebreakers")
+      .insertOne(newIceBreaker);
+    res.status(201);
+    res.json(newIceBreaker);
+  } catch (err) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 
 //PUT
@@ -58,19 +70,25 @@ IcebreakerRoute.put("/Icebreakers/:id", async (req, res) => {
       res.status(404).json({ message: "Not Found" });
     } else data._id = new ObjectId(id);
     res.json(data);
-  } catch {}
+  } catch (err) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 
 //Delete
 IcebreakerRoute.delete("/icebreakers/:id", async (req, res) => {
   const id = req.params.id;
-  const client = await getClient();
-  const result = await client
-    .db()
-    .collection<IceBreaker>("icebreakers")
-    .deleteOne({ _id: new ObjectId(id) });
-  if (result.deletedCount === 0) {
-    res.status(404).json({ message: "Not Found" });
-  } else res.status(204).end();
+  try {
+    const client = await getClient();
+    const result = await client
+      .db()
+      .collection<IceBreaker>("icebreakers")
+      .deleteOne({ _id: new ObjectId(id) });
+    if (result.deletedCount === 0) {
+      res.status(404).json({ message: "Not Found" });
+    } else res.status(204).end();
+  } catch (err) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 export default IcebreakerRoute;
