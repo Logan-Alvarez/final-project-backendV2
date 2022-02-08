b2a1d7184d69e98a80318b588a51f830fb14f242
stovet <steven.topalian@gmail.com>
2022-02-08 13:24:11 -0500

    joke model and route

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,11 +2,13 @@ import * as functions from "firebase-functions";
 import express from "express";
 import cors from "cors";
 import IcebreakerRoute from "./routes/IcebreakerRoute";
+import favoriteJoke from './routes/favoriteJokes';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 app.use("/", IcebreakerRoute);
+app.use("/", favoriteJoke);
 
 export const api = functions.https.onRequest(app);
